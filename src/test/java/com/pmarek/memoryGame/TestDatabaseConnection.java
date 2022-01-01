5ef75cc20877985d6919a93d4e5649e412ec8a68
Przemysław Marek <przemyslawmarek516@gmail.com>
2022-01-01 13:49:13 +0100

    DatabaseConnection tests

--- src/test/java/com/pmarek/memoryGame/TestDatabaseConnection.java
+++ src/test/java/com/pmarek/memoryGame/TestDatabaseConnection.java
@@ -1,15 +1,33 @@
 package com.pmarek.memoryGame;
 
 import com.pmarek.memoryGame.utils.DatabaseConnection;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
 
 
 public class TestDatabaseConnection {
 
+    private DatabaseConnection databaseConnection;
+
+    @BeforeClass
+    public void init(){
+        databaseConnection = new DatabaseConnection();
+    }
+
+    @After public void deleteDatabase(){
+        File file = new File("results.db");
+        file.delete();
+    }
+
     @Test
     public void shouldReturnTrueWhenDatabaseIsCreated(){
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Assert.assertNotNull(DatabaseConnection.getConn());
+        Assert.assertTrue(databaseConnection.isConnectionEstabilished());
+    }
+
+    @Test
+    public void asdasd(){
+
     }
 }
